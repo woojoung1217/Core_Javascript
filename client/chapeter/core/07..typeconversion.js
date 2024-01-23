b5099a57dd61c80be02cbb9c2aa3d05cdf3da584
woojoung <ywj981217@naver.com>
2024-01-23 11:14:52 +0900

    day07 study

---
+++ client/chapeter/core/07..typeconversion.js
@@ -0,0 +1,55 @@
+/* --------------------- */
+/* Type Conversion       */
+/* --------------------- */
+
+
+/* 데이터 → 문자 ----------------------------------------------------------- */
+
+// number
+
+const YEAR = 2024;
+
+console.log(typeof String(YEAR))  // 명시적 
+console.log(YEAR + ' ')  // 암시적
+
+
+
+
+// undefined, null
+
+let days = null;
+
+let weekend;
+
+
+// console.log(days + '');
+
+
+// boolean
+
+let isClicked = false;
+
+
+
+/* 데이터 → 숫자 ----------------------------------------------------------- */
+
+// undefined
+
+// null
+
+// boolean
+
+// string
+
+// numeric string
+
+
+/* 데이터 → 불리언 ---------------------------------------------------------- */
+
+// null, undefined, 0, NaN, ''
+// 위에 나열한 것 이외의 것들 
+
+// const value = prompt('입력'
+// )
+
+// console.log(parseInt(value) + 50)
